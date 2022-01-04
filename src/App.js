54bf4dab0366af2cae591f9a1315256e9d5f7438
IcarusDrowns <73529048+IcarusDrowns@users.noreply.github.com>
2022-01-04 08:29:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const fetch = async (e) => {
     e.preventDefault();
     const response = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=49cc8c821cd2aff9af04c9f98c36eb74`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9cacdfdab391b52dc15c9197b29942eb`
     );
     setWeather(response.data);
   };
